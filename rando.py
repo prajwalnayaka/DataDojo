3df26b6728438df972a1010859f7f433694b2b03
Prajwal Nayaka T <prajwalnayakat@gmail.com>
2026-03-31 18:07:00 +0530

    Delete rando.py

--- rando.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-ds=pd.read_csv(r"D:\DataDojo\Industrial\iot_telemetry_data.csv")
-mapping={True:"yes",False:"no"}
-ds['motion']=ds['motion'].map(mapping)
-ds.to_csv(r"D:\DataDojo\Industrial\iot_telemetry_data.csv")
