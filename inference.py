9b6fd14e7ae2321facc56171ce2b3ef6bc9e0fcf
Prajwal Nayaka <prajwalnayakat@gmail.com>
2026-04-08 15:09:47 +0530

    Update inference.py

--- inference.py
+++ inference.py
@@ -28,7 +28,7 @@ load_dotenv()
 # ==========================================
 
 IMAGE_NAME = os.getenv("LOCAL_IMAGE_NAME")
-API_KEY = os.getenv("HF_TOKEN") or os.getenv("API_KEY")
+API_KEY = os.getenv("HF_TOKEN")
 API_BASE_URL = os.getenv("API_BASE_URL") or "https://router.huggingface.co/v1"
 MODEL_NAME = os.getenv("MODEL_NAME") or "Qwen/Qwen2.5-72B-Instruct"
 DIFFICULTIES = ["Easy", "Medium", "Hard"]
